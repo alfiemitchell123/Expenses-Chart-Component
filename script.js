9ee450f2ea233013a4048b9fbc8ab432ca584165
alfiemitchell123 <alfiemitchell1@gmail.com>
2023-07-24 17:30:01 -0700

    updated most of the site. still need to add balance element

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Fetch w/ .then is one way of fetching data from a JSON file / API
+// Fetch w/ .then is one way of fetching data from a JSON file / API (save for future reference)
 // fetch('/data.json')
 //     .then(res => {
 //         return res.json();
@@ -24,19 +24,42 @@ async function fetchData() {
     return data;
 }
 
+// Display expense data in the graph
 async function tableData() {
     try {
         const jsonData = await fetchData();
 
         jsonData.forEach(expense => {
-            const day = `<tr class="day"><th scope="row">${expense.day}</th></tr>`;
-            document.querySelector('tbody').insertAdjacentHTML('beforeend', day);
+            const amount = `<div class="day"><p class="p-amount">${expense.amount}</p></div>`;
+            document.querySelector('ul').insertAdjacentHTML('beforeend', amount);
 
             const dayElements = document.querySelectorAll('.day');
             const lastDayElement = dayElements[dayElements.length - 1];
 
-            const amount = `<td class="amount"><p>${expense.amount}</p></td>`;
-            lastDayElement.insertAdjacentHTML('beforeend', amount);
+            const day = `<li class="li-day">${expense.day}</li>`;
+            lastDayElement.insertAdjacentHTML('beforeend', day);
+
+            const pElements = document.querySelectorAll('.p-amount');
+            const lastPElement = pElements[pElements.length - 1];
+
+            const bar = `<div class="bar" style="height:${(expense.amount) * 3}px"></div>`;
+            lastPElement.insertAdjacentHTML('afterend', bar);
+
+            ////////////////////////////////////////////////////////////////////////////////
+
+            const barElements = document.querySelectorAll('.bar');
+
+            barElements.forEach(function (barElement, index) {
+                barElement.addEventListener('mouseover', function () {
+                    pElements[index].style.opacity = "1";
+                });
+            })
+
+            barElements.forEach(function (barElement, index) {
+                barElement.addEventListener('mouseout', function () {
+                    pElements[index].style.opacity = "0";
+                });
+            })
         });
     } catch (error) {
         console.log('Error fetching/parsing JSON:', error);
@@ -44,17 +67,17 @@ async function tableData() {
 }
 tableData();
 
+// Add the sum of the expenses and insert into the total for the month
+// async function calculateSum() {
+//     try {
+//         const jsonData = await fetchData();
 
-async function calculateSum() {
-    try {
-        const jsonData = await fetchData();
+//         const totalSum = jsonData.reduce((acc, expense) => acc + expense.amount, 0);
 
-        const totalSum = jsonData.reduce((acc, expense) => acc + expense.amount, 0);
-
-        const total = `Total: ${totalSum}`
-        document.querySelector('p').insertAdjacentHTML('beforeend', total);
-    } catch (error) {
-        console.log('Error fetching/parsing JSON:', error);
-    }
-}
-calculateSum();
+//         const total = `$${totalSum}`
+//         document.querySelector('#total-for-month').insertAdjacentHTML('beforeend', total);
+//     } catch (error) {
+//         console.log('Error fetching/parsing JSON:', error);
+//     }
+// }
+// calculateSum();
